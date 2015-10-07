b999b674e29474ff28b492df63397d2aab33c722
dbxinj <jixin@comp.nus.edu.sg>
2015-10-07 21:11:55 +0800

    add option rules to extract_feature.sh

--- examples/tianchi/extract_feature.sh
+++ examples/tianchi/extract_feature.sh
@@ -1,9 +1,15 @@
-myPath="examples/_temp"
-if [ ! -d "$myPath" ];
-then
-	mkdir "$myPath"
+if [ $# -lt 5 ]; then
+	echo "Usage: ./extract_feature.sh model_path cnn_path blob_name feature_path batch_size"
+	echo "  e.g., ./extract_feature.sh models/caffemodel tianchi/tianchi.prototxt fc7 features 10"
+	echo "--------------------------------------------------------------------------------------"
+	echo "  model_path should be exact where neral net model locates"
+	echo "  cnn_path shoulb be exact where cnn layers proto text locates"
+	echo "  feature_path is where you want to save the extracted features"
+	echo "  blob_name is layer name defined in proto text"
+	echo "  batch_size is used to batch the cnn model"
+	exit
 fi
 
 #find /data/jixin/tianchi/resized_10k -type f -exec echo {} \; > $myPath/temp.txt
 #sed "s/$/ 0/" $myPath/temp.txt > $myPath/file_list.txt
-./build/tools/extract_features.bin models/bvlc_reference_caffenet/bvlc_reference_caffenet.caffemodel examples/_temp/tianchi_val.prototxt fc7 examples/_temp/features_tc 10 lmdb
+./build/tools/extract_features.bin $1 $2 $3 $4 $5 lmdb
