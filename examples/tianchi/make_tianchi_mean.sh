38d372a3a8ed3fd3733353bc3eb6cd7523fd717f
dbxinj <jixin@comp.nus.edu.sg>
2015-10-07 21:12:15 +0800

    add option rules to make_tianchi_mean.sh

--- examples/tianchi/make_tianchi_mean.sh
+++ examples/tianchi/make_tianchi_mean.sh
@@ -1,7 +1,17 @@
-EXAMPLE=examples/tianchi
+if [ $# -lt 2 ]; then
+	echo "Usage: make_tianchi_mean.sh lmdbfilename meanfilename"
+	echo "  e.g., make_tianchi_mean.sh train_lmdb train_mean.binaryproto"
+	echo "------------------------------------------------------------------"
+	echo "  lmdb file should be placed in folder /data/jixin/tianchi/"
+	echo "  mean file will be generated in folder data/tianchi/"
+	exit
+fi
+
+#EXAMPLE=examples/tianchi
+EXAMPLE=/data/jixin/tianchi
 DATA=data/tianchi
 TOOLS=build/tools
 
-$TOOLS/compute_image_mean $EXAMPLE/tianchi_train_lmdb $DATA/tianchi_mean.binaryproto
+$TOOLS/compute_image_mean $EXAMPLE/$1 $DATA/$2
 
 echo "done."
