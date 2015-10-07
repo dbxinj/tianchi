981204b0210431221a46569296907922e093302f
dbxinj <jixin@comp.nus.edu.sg>
2015-10-07 21:10:04 +0800

    add option rules to create_tianchi.sh

--- examples/tianchi/create_tianchi.sh
+++ examples/tianchi/create_tianchi.sh
@@ -2,11 +2,23 @@
 # Create the imagenet lmdb inputs
 # N.B. set the path to the imagenet train + val data dirs
 
-EXAMPLE=examples/tianchi
+EXAMPLE=/data/jixin/tianchi
+#EXAMPLE=examples/tianchi
 DATA=data/tianchi
 TOOLS=build/tools
 
-TRAIN_DATA_ROOT=/data/jixin/tianchi/train/
+if [ $# -lt 2 ]; then
+	echo "Usage: create_tianchi.sh srcfile_list dst_lmdb"
+	echo "  e.g., create_tianchi.sh train.txt train_lmdb"
+	echo "---------------------------------------------------"
+	echo "  srcfile_list contains all the imagefile names."
+	echo "  srcfile_list should be placed in data/tianchi/"
+	echo "  imagefiles will be converted to lmdb and"
+	echo "  stored in /data/jixin/tianchi/dst_lmdb."
+	exit
+fi
+
+TRAIN_DATA_ROOT="/data/jixin/tianchi/resized_10k/"
 #VAL_DATA_ROOT=/path/to/imagenet/val/
 
 # Set RESIZE=true to resize the images to 256x256. Leave as false if images have
@@ -41,8 +53,8 @@ GLOG_logtostderr=1 $TOOLS/convert_imageset \
     --resize_width=$RESIZE_WIDTH \
     --shuffle \
     $TRAIN_DATA_ROOT \
-    $DATA/train.txt \
-    $EXAMPLE/tianchi_train_lmdb
+    $DATA/$1 \
+    $EXAMPLE/$2
 
 #echo "Creating val lmdb..."
 
